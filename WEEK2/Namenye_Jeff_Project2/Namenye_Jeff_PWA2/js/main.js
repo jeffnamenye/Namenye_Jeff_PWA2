70325d1e0f635bfda4bacc60ec681c48fac31563
Jeff Namenye <jeffnamenye@hotmail.com>
2015-01-15 08:40:15 -0500

    First

--- WEEK2/Namenye_Jeff_Project2/Namenye_Jeff_PWA2/js/main.js
+++ WEEK2/Namenye_Jeff_Project2/Namenye_Jeff_PWA2/js/main.js
@@ -4,15 +4,34 @@
 */
 
 (function($){
-	
-	
+
+    /*========== tooltip =========*/
+
+    $(".masterTooltip").hover(function(){
+            //Hover over
+            var title = $(this).attributes('title');
+            $(this).data('tipText',title).removeAttribute('title');
+            $('<p class="tooltip"></p>')
+                .text(title)
+                .appendTo('body')
+                .fadeIn('slow');
+        },
+        function(){
+            //Hover out
+            $(this).attributes("title",$(this).data("tipText"));
+            $(".toolTip").remove();
+        }) .mousemove(function(e) {
+            var mousex = e.pageX + 20;
+            var mousey = e.pageY + 10;
+            $('.tooltip')
+                .css({ top:mousey, left:mousex})
+
+        });
 	
 	
 		
 
 	
 })(jQuery); // end private scope
-
-
 
 
